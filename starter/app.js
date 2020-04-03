accc5cbbda975548cf5987cff10cee0515064dd1
Akshay <apple@Apples-MacBook-Air.local>
2020-04-03 21:23:13 +0530

    Version 1.0

---
+++ starter/app.js
@@ -0,0 +1,110 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activeplayer, gamePlaying;
+init();
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+	if(gamePlaying){
+		// 1. Random Number
+		var dice = Math.floor(Math.random() * 6) + 1;
+
+		// 2. Display the result
+		var diceDOM = document.querySelector('.dice');
+		diceDOM.style.display = 'block';
+		diceDOM.src = 'dice-' + dice + '.png'; 
+
+		//3. Update the roundscore If the rolled number is NOT a 1
+		if(dice !== 1){
+			// Add score
+			// roundScore += dice;
+			roundScore = roundScore + dice;
+			// console.log(roundScore);
+			document.querySelector('#current-' + activeplayer).textContent = roundScore;
+		} else{
+			// Next player
+			nextPlayer();
+		}
+	}
+
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+	if(gamePlaying){
+		// add current score to the GLOBAL score
+		// scores[activeplayer] += roundScore;
+		scores[activeplayer] = scores[activeplayer] + roundScore;
+
+		// Update the UI
+		document.querySelector('#score-' + activeplayer).textContent = scores[activeplayer];
+
+		// check if the player is won
+		if(scores[activeplayer] >= 50){
+			document.querySelector('#name-' + activeplayer).textContent = 'Winner';
+			document.querySelector('.dice').style.display = 'none';
+			document.querySelector('.player-'+ activeplayer + '-panel').classList.add('winner');
+			document.querySelector('.player-'+ activeplayer + '-panel').classList.remove('active');
+			gamePlaying = false;
+		} else {
+			// Next player
+			nextPlayer();
+		}
+	}
+
+});
+
+
+// New Game CLick
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+function init(){
+	scores = [0, 0];
+	roundScore = 0;
+	activeplayer = 0;	
+	gamePlaying = true;
+
+	document.querySelector('.dice').style.display = 'none';
+
+	document.getElementById('score-0').textContent = 0;
+	document.getElementById('score-1').textContent = 0;
+	document.getElementById('current-0').textContent = 0;
+	document.getElementById('current-1').textContent = 0;
+	document.getElementById('name-0').textContent = 'Player 1';
+	document.getElementById('name-1').textContent = 'Player 2';
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+	document.querySelector('.player-0-panel').classList.add('active');
+}
+
+
+
+	function nextPlayer(){
+		// Next player
+		activeplayer === 0 ? activeplayer = 1 : activeplayer = 0;
+		roundScore = 0;
+
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+
+		document.querySelector('.dice').style.display = 'none';
+	}
+
+
+
+
+
